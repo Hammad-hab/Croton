beef5f9f8cb743aca01eb1a0fdc7f3ef06331202
CW team <>
2023-10-14 18:21:39 +0500

    Finalized v0.1

--- croton_install.sh
+++ croton_install.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 echo "Installing Croton"
+curl -L -o crotonc https://github.com/Hammad-hab/Croton/raw/main/build/crotonc
+chmod +x ./crotonc
+echo "Successfully installed Croton..."
+sudo mv ./croton /usr/local/bin/
