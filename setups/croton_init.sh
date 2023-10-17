3af62fef5b54fc3c267cc37f7346aa5b98710386
CW team <>
2023-10-17 16:01:01 +0500

    Altered Stdlib

---
+++ setups/croton_init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+su
+cd ./src/
+zip -r ./__build/lib.zip ./lib
+cd ../
+git add .
+git commit -m "Altered Stdlib"
+git push -f origin main
+rm -rf /usr/local/croton_
+python3 ./setup_libs.py
